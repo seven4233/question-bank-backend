27db49ca05e09b7f11b987527c29ee3329f150fc
hl <hl_taishi@qq.com>
2024-06-15 23:21:46 +0800

    拿掉Secret

--- src/utils/cos.ts
+++ src/utils/cos.ts
@@ -1,6 +1,6 @@
 import * as COS from 'cos-nodejs-sdk-v5'
 
 export const cos = new COS({
-    SecretId: '', // 推荐使用环境变量获取；用户的 SecretId，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参考https://cloud.tencent.com/document/product/598/37140
-    SecretKey: '', // 推荐使用环境变量获取；用户的 SecretKey，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参考https://cloud.tencent.com/document/product/598/37140
+    // SecretId: '', // 推荐使用环境变量获取；用户的 SecretId，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参考https://cloud.tencent.com/document/product/598/37140
+    // SecretKey: '', // 推荐使用环境变量获取；用户的 SecretKey，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参考https://cloud.tencent.com/document/product/598/37140
 });
